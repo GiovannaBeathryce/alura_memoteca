0a6cf3d8bd03b96b9d0ce29d9bd6e3155fa41025
Giovanna Oliveira <giovannabeathryce.pso@gmail.com>
2023-06-01 19:03:26 -0300

    Validated creation form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ThoughtWallComponent } from './components/thoughts/thought-wall/thought-wall.component';
 import { ThoughtComponent } from './components/thoughts/thought/thought.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -28,7 +28,8 @@ import { UpdateThoughtComponent } from './components/thoughts/update-thought/upd
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -2,6 +2,7 @@ import { ThoughtService } from './../thought.service';
 import { Pensamento } from './../thought';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-thought',
@@ -9,24 +10,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-thought.component.css']
 })
 export class CreateThoughtComponent {
-  pensamento : Pensamento = {
-    conteudo:'',
-    autoria:'',
-    modelo:''
-  }
+
+  formulario! : FormGroup
 
   constructor(
     private service:ThoughtService,
-    private router: Router
+    private router: Router,
+    private formBuilder : FormBuilder
   ){}
 
-  criarPensamento(){
-    this.service.create(this.pensamento).subscribe(() => {
-      this.router.navigate(["/pensamentos"])
+  ngOnInit() : void {
+    this.formulario = this.formBuilder.group({
+      conteudo:['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
+      modelo: ['modelo1']
     })
+  }
+
+  criarPensamento(){
+    console.log(this.formulario.get('autoria')?.errors);
+
+    if(this.formulario.valid){
+        this.service.create(this.formulario.value).subscribe(() => {
+        this.router.navigate(["/pensamentos"])
+      })
+    }
   }
 
   cancelaCriacao(){
     this.router.navigate(["/pensamentos"])
+  }
+
+  btnIsActibe() : string {
+    if(this.formulario.valid){
+      return 'botao'
+    }
+    return 'btn_disabled'
   }
 }
